88ac5cf8f7df9b5a8462a42d4e175f4e294d94aa
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2020-06-02 14:30:15 +0200

    jpg to webp for gallery

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ print("Process images")
 
 for filename in os.listdir("img/sketchnotes"):
     print(filename)
-    os.system("mogrify -resize 300x300 -quality 75 \-format jpg -write static/img/sketchnotes/" + os.path.splitext(filename)[0] + "-mini.jpg img/sketchnotes/" + filename)
+    os.system("mogrify -resize 300x300 -quality 75 \-format webp -write static/img/sketchnotes/" + os.path.splitext(filename)[0] + "-mini.webp img/sketchnotes/" + filename)
     os.system("cp img/sketchnotes/" + filename + " static/img/sketchnotes/"+ os.path.splitext(filename)[0] + "-full.jpg")
 
 
